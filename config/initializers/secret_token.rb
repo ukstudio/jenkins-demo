ce83340cd0a8215998d264ad929cd4eea6e543a6
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-06-29 12:09:56 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsDemo::Application.config.secret_token = 'a7c35f8bab1033ac597b9d9ac586d1b3324928499763e23f791aa2e2c5c30eff31a4db0e202bde7be57883ffedb809f450fb17d0efc1560a90d0373b66bfef17'
